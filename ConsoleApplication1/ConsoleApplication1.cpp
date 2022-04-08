64f667593c663388ba20e3067dbed7e27cc17869
void-daniel <51021466+pwrdaniel@users.noreply.github.com>
2022-04-08 11:37:59 +0200

    Updated 8.04.2022 - 11:37
    Inheritance in CPP

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ public:
     int m_age = {};
 
     Person(const string& name = "", int age = 0)
-        : m_name{ name }, m_age{ age }
+ : this->m_name{ name }, this->m_age{ age }
     {
     }
 
@@ -26,7 +26,7 @@ public:
     int m_homeRuns{};
 
     BaseballPlayer(double battingAverage = 0.0, int homeRuns = 0)
-        : m_battingAverage{ battingAverage }, m_homeRuns{ homeRuns }
+        : this->m_battingAverage{ battingAverage }, this->m_homeRuns{ homeRuns }
     {
     }
 
@@ -35,31 +35,34 @@ public:
 class Employee : public Person 
 {
 public:
-    double m_placa = {};
+    double m_salary = {};
 
-    Employee(double placa = 0.0) : m_placa{placa} {}
+    Employee(double salary = 0.0) : this->m_salary{salary} {}
+
+    double getSalary() : return{m_salary}
 };
 
 
 int main()
 {
     BaseballPlayer john;
-    john.m_name = "John"; // Našemu igralcu je ime John
+    john.m_name = "John"; // Our guys name is John
     john.m_age = 27; 
     john.m_battingAverage = 127.8;
     
-    //Izpis bo vseboval atribute iz obeh knjiznic
+    //Output contains from both classes that we inherited
     cout << john.getName() << "\n" << john.getAge() << "\n" << john.m_battingAverage;
     cout << endl;
 
-
+    //Without the functions output
     Employee employee;
     employee.m_name = "Janez";
     employee.m_age = 19;
-    employee.m_placa = 724.89;
+    employee.m_salary = 724.89;
 
+    //Output with the functions
     cout << employee.getName() << endl;
     cout << employee.getAge() << endl;
-    cout << employee.m_placa << endl;
+    cout << employee.getSalary() << endl;
 }
 
